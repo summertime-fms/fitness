907aec47920d48e34042275e9003962159f50ae1
summertime-fms <danyushin0107@gmail.com>
2020-06-05 20:18:41 +0300

    Разметка блока промо

--- source/js/script.js
+++ source/js/script.js
@@ -19,21 +19,38 @@ const toggler = document.querySelector(".catalog__toggler");
 
 tabs.onclick = function(evt) {
   evt.preventDefault();
-  let tabsClientRect = this.getBoundingClientRect();
-  let tabsBorderLeft = tabsClientRect.x;
+
+  let itemGroups = document.getElementsByClassName("catalog__items-group")
   let target = event.target.closest("li");
   if (!target) return;
 
+  let tabsClientRect = this.getBoundingClientRect();
+  let tabsBorderLeft = tabsClientRect.left;
   let tabClientRect = target.getBoundingClientRect();
   let tabWidth = tabClientRect.width + "px";
 
   let targetClientRect = target.getBoundingClientRect();
-  let leftBorder = targetClientRect.x;
+  let leftBorder = targetClientRect.left;
   let x = leftBorder - tabsBorderLeft + "px";
 
   toggler.style.left = x;
+  toggler.style.transition = "left 0.1s ease-in";
+
   toggler.style.width = tabWidth;
 
-
-
+  if (target.classList.contains("catalog__tab--second")) {
+    itemGroups[0].style.display = "none";
+    itemGroups[1].style.display = "flex";
+    itemGroups[2].style.display = "none";
+  }
+  if (target.classList.contains("catalog__tab--first")) {
+    itemGroups[0].style.display = "flex";
+    itemGroups[1].style.display = "none";
+    itemGroups[2].style.display = "none";
+  }
+  if (target.classList.contains("catalog__tab--third")) {
+    itemGroups[0].style.display = "none";
+    itemGroups[1].style.display = "none";
+    itemGroups[2].style.display ="flex";
+  }
 }
