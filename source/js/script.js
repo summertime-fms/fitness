b88f5187c9d1c0b7adbee5147edfbdbaed22dd47
summertime-fms <danyushin0107@gmail.com>
2020-06-05 18:31:53 +0300

    оживляет переключатель карточек, анимирует контролл

--- source/js/script.js
+++ source/js/script.js
@@ -13,3 +13,27 @@ mainBlockLink.addEventListener("click", function(evt) {
 
   return false;
 });
+
+const tabs = document.querySelector(".catalog__tabs");
+const toggler = document.querySelector(".catalog__toggler");
+
+tabs.onclick = function(evt) {
+  evt.preventDefault();
+  let tabsClientRect = this.getBoundingClientRect();
+  let tabsBorderLeft = tabsClientRect.x;
+  let target = event.target.closest("li");
+  if (!target) return;
+
+  let tabClientRect = target.getBoundingClientRect();
+  let tabWidth = tabClientRect.width + "px";
+
+  let targetClientRect = target.getBoundingClientRect();
+  let leftBorder = targetClientRect.x;
+  let x = leftBorder - tabsBorderLeft + "px";
+
+  toggler.style.left = x;
+  toggler.style.width = tabWidth;
+
+
+
+}
