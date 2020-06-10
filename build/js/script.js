eb5469d115e64e3b1308e1b604b5e3775a2793fe
summertime-fms <danyushin0107@gmail.com>
2020-06-11 00:04:34 +0300

    исправляет отображение в IE11 и Mozilla

--- build/js/script.js
+++ build/js/script.js
@@ -1,3 +1,58 @@
+// ******Polyfills******
+(function() {
+
+  if (!Element.prototype.matches) {
+
+    Element.prototype.matches = Element.prototype.matchesSelector ||
+      Element.prototype.webkitMatchesSelector ||
+      Element.prototype.mozMatchesSelector ||
+      Element.prototype.msMatchesSelector;
+
+  }
+
+})();
+(function() {
+
+
+  if (!Element.prototype.closest) {
+    Element.prototype.closest = function(css) {
+      var node = this;
+
+      while (node) {
+        if (node.matches(css)) return node;
+        else node = node.parentElement;
+      }
+      return null;
+    };
+  }
+
+})();
+// *********
+
+$( document ).ready(function(){
+function getMaxHeight() {
+  var comments = document.getElementsByClassName("comments__slide");
+
+  var maxHeight = 0;
+
+      for (var i = 0; i < comments.length; i++) {
+        let elemHeight = getHeight(comments[i])
+
+        if (elemHeight > maxHeight) {
+          maxHeight = elemHeight;
+
+        }
+      }
+
+  function getHeight(element) {
+    let styles = getComputedStyle(element);
+    let height = parseInt(styles.height);
+    return height;
+  }
+return maxHeight
+}
+
+
 const mainBlockLink = document.querySelector(".intro__button");
 
 mainBlockLink.addEventListener("click", function(evt) {
@@ -55,22 +110,58 @@ tabs.onclick = function(evt) {
   }
 }
 
-// const nextButton = document.querySelector(".coaches__button-next");
-// const slider = document.querySelector(".coaches__slider");
-// const sliderWrapper = document.querySelector(".coaches__slider-wrapper");
 
-// nextButton.onclick = function() {
-//   let sliderWrpClientRect = sliderWrapper.getBoundingClientRect();
-//   let sliderWrpWidth = sliderWrpClientRect.width;
-//   let x = -sliderWrpWidth + "px";
+                // ***Sliders***
 
-//   let sliderClientRect = slider.getBoundingClientRect();
-//   let sliderLeft = sliderClientRect.x;
-//   console.log(sliderLeft);
+var mySwiper = new Swiper ('.coaches__slider-wrapper', {
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: '.coaches__button-next',
+    prevEl: '.coaches__button-prev',
+  },
 
-//   if (sliderLeft < 371.6 && sliderLeft > -828.4)  {
-//   slider.style.left = x;
-//   } else  {
+  spaceBetween: 40,
 
-//   }
-// }
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      slidesPerGroup: 1,
+      width:226
+    },
+    768: {
+       slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 30,
+      width: 536
+
+    },
+    1200: {
+      slidesPerGroup: 4,
+      slidesPerView: 4,
+      spaceBetween: 40,
+      width: 1160
+
+    }
+
+
+  }
+})
+
+var mySwiper1 = new Swiper('.comments__slider', {
+  direction: 'horizontal',
+  loop: true,
+  height: getMaxHeight(),
+  navigation: {
+    nextEl: '.comments__button-next',
+    prevEl: '.comments__button-prev',
+  },
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+
+  spaceBetween: 0,
+
+});
+});
